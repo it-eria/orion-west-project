c6417b924d44316576c32eafa38f715287eea701
it-eria <iteria.info@gmail.com>
2018-03-09 17:11:26 +0200

    fix all bugs

---
+++ js/scripts.js
@@ -0,0 +1,208 @@
+$(".scrollTo").on('click', function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: ($(target).offset().top)}, 1500)
+});
+
+
+    $('.burger').click(function() {
+        if(!$('.burger').hasClass('cross'))
+            $('.burger').addClass('cross');
+        else
+            $('.burger').removeClass('cross');
+    });
+
+
+
+$(document).ready(function () {
+    var didScroll;
+    var lastScrollTop = 100;
+    var delta = 5;
+    var navbarHeight = $('#navb').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight) {
+
+            $('#navb').removeClass('menu-scroll').addClass('nav-up');
+        } else {
+
+            if (st + $(window).height() < $(document).height()) {
+                $('#navb').removeClass('nav-up').addClass('menu-scroll');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+    var $slider = $(".slider"),
+        $slideBGs = $(".slide__bg"),
+        diff = 0,
+        curSlide = 0,
+        numOfSlides = $(".slide").length - 1,
+        animating = false,
+        animTime = 500,
+        autoSlideTimeout,
+        autoSlideDelay = 6000,
+        $pagination = $(".slider-pagi");
+
+    function createBullets() {
+        for (var i = 0; i < numOfSlides + 1; i++) {
+            var $li = $("<li class='slider-pagi__elem'></li>");
+            $li.addClass("slider-pagi__elem-" + i).data("page", i);
+            if (!i) $li.addClass("active");
+            $pagination.append($li);
+        }
+    };
+
+    createBullets();
+
+    function manageControls() {
+        $(".slider-control").removeClass("inactive");
+        if (!curSlide) $(".slider-control.left").addClass("inactive");
+        if (curSlide === numOfSlides) $(".slider-control.right").addClass("inactive");
+    };
+
+    function autoSlide() {
+        autoSlideTimeout = setTimeout(function () {
+            curSlide++;
+            if (curSlide > numOfSlides) curSlide = 0;
+            changeSlides();
+        }, autoSlideDelay);
+    };
+
+    autoSlide();
+
+    function changeSlides(instant) {
+        if (!instant) {
+            animating = true;
+            manageControls();
+            $slider.addClass("animating");
+            $slider.css("top");
+            $(".slide").removeClass("active");
+            $(".slide-" + curSlide).addClass("active");
+            setTimeout(function () {
+                $slider.removeClass("animating");
+                animating = false;
+            }, animTime);
+        }
+        window.clearTimeout(autoSlideTimeout);
+        $(".slider-pagi__elem").removeClass("active");
+        $(".slider-pagi__elem-" + curSlide).addClass("active");
+        $slider.css("transform", "translate3d(" + -curSlide * 100 + "%,0,0)");
+        $slideBGs.css("transform", "translate3d(" + curSlide * 50 + "%,0,0)");
+        diff = 0;
+        autoSlide();
+    }
+
+    function navigateLeft() {
+        if (animating) return;
+        if (curSlide > 0) curSlide--;
+        changeSlides();
+    }
+
+    function navigateRight() {
+        if (animating) return;
+        if (curSlide < numOfSlides) curSlide++;
+        changeSlides();
+    }
+
+    $(document).on("mousedown touchstart", ".slider", function (e) {
+        if (animating) return;
+        window.clearTimeout(autoSlideTimeout);
+        var startX = e.pageX || e.originalEvent.touches[0].pageX,
+            winW = $(window).width();
+        diff = 0;
+
+        $(document).on("mousemove touchmove", function (e) {
+            var x = e.pageX || e.originalEvent.touches[0].pageX;
+            diff = (startX - x) / winW * 70;
+            if ((!curSlide && diff < 0) || (curSlide === numOfSlides && diff > 0)) diff /= 2;
+            $slider.css("transform", "translate3d(" + (-curSlide * 100 - diff) + "%,0,0)");
+            $slideBGs.css("transform", "translate3d(" + (curSlide * 50 + diff / 2) + "%,0,0)");
+        });
+    });
+
+    $(document).on("mouseup touchend", function (e) {
+        $(document).off("mousemove touchmove");
+        if (animating) return;
+        if (!diff) {
+            changeSlides(true);
+            return;
+        }
+        if (diff > -8 && diff < 8) {
+            changeSlides();
+            return;
+        }
+        if (diff <= -8) {
+            navigateLeft();
+        }
+        if (diff >= 8) {
+            navigateRight();
+        }
+    });
+
+    $(document).on("click", ".slider-control", function () {
+        if ($(this).hasClass("left")) {
+            navigateLeft();
+        } else {
+            navigateRight();
+        }
+    });
+
+    $(document).on("click", ".slider-pagi__elem", function () {
+        curSlide = $(this).data("page");
+        changeSlides();
+    });
+
+});
+
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(".navbar").css('background-color', 'rgba(89, 101, 111, 0.85)');
+            } else {
+                $('.navbar').css('background-color', 'transparent');
+            }
+        });
+    }
+
+
+    var scroll_start1 = 0;
+    var startchange1 = $('#startchange1');
+    var offset1 = startchange1.offset();
+    if (startchange1.length) {
+        $(document).scroll(function () {
+            scroll_start1 = $(this).scrollTop();
+            if (scroll_start1 > offset1.top) {
+                $(".other-menu").css('background-color', '#fff');
+            } else {
+                $('.other-menu').css('background-color', '#fff');
+            }
+        });
+    }
+});
+
+
+
