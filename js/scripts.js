cfdbfe609d321eba3a4faefa0a3947e017a74af4
it-eria <iteria.info@gmail.com>
2018-03-23 19:11:50 +0200

    add clients block

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
+$('.clients-blocks').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    focusOnSelect: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+       ]
+});
+
 $(".scrollTo").on('click', function (e) {
     e.preventDefault();
     var target = $(this).attr('href');
@@ -203,6 +236,8 @@ $(document).ready(function () {
         });
     }
 });
+
+
 
 
 
